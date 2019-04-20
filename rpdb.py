56de0eb9d866e300fda19bb3f7f01442f5cba024
Pau <pausampietro@gmail.com>
2019-04-20 13:19:49 +0200

    Changed files structure, DATA READY in DB, started descriptive STATS

---
+++ rpdb.py
@@ -0,0 +1,17 @@
+# imports
+import pymysql
+from sqlalchemy import create_engine
+import pandas as pd
+
+# Function to read a table from the database
+def read_table(table):
+	cs  = 'mysql+pymysql://root:root@35.246.218.28/run_moves'
+	engine = create_engine(cs)
+	Ssql = f'SELECT * FROM {table}'
+	return pd.read_sql(Ssql,engine)
+
+	
+def export_table(dataframe, table, METHOD='replace'):
+	cs  = 'mysql+pymysql://root:root@35.246.218.28/run_moves'
+	engine = create_engine(cs)
+	dataframe.to_sql(con=engine, name=table,  if_exists= METHOD)
